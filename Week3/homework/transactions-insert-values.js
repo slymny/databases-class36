9e67c773e24e2bc88ceccbfafeeb6e058d6c47a6
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-06-01 12:33:00 +0200

    Inserts are converted to prepared statements.

--- Week3/homework/transactions-insert-values.js
+++ Week3/homework/transactions-insert-values.js
@@ -11,26 +11,37 @@ const connection = mysql.createConnection({
 const execQuery = util.promisify(connection.query.bind(connection));
 
 async function seedDatabase() {
+  const accountsToInsert = [
+    [101, 10000],
+    [102, 5000],
+    [103, 267],
+    [104, 6590],
+    [105, 350],
+  ];
+
+  const changesToInsert = [
+    [104, 740, '2022-10-10', 'Going EFT'],
+    [105, 740, '2022-10-10', 'Coming EFT'],
+  ];
   const INSERT_ACCOUNTS = `
     INSERT INTO accounts (account_number, balance)
-        VALUES 
-            (101, 10000),
-            (102, 5000),
-            (103, 267),
-            (104, 6590),
-            (105, 350);`;
+        VALUES (?, ?)`;
 
   const INSERT_ACCOUNT_CHANGES = `
     INSERT INTO account_changes (account_number, amount, changed_date, remark)
-        VALUES
-        (104, 740, '2022-10-10', "Going EFT"),
-        (105, 740, '2022-10-10', "Coming EFT");`;
+        VALUES (?, ?, ?, ?);`;
 
   connection.connect();
 
   try {
-    await execQuery(INSERT_ACCOUNTS);
-    await execQuery(INSERT_ACCOUNT_CHANGES);
+    // await execQuery(INSERT_ACCOUNTS);
+    accountsToInsert.forEach(
+      async value => await execQuery(INSERT_ACCOUNTS, value),
+    );
+    // await execQuery(INSERT_ACCOUNT_CHANGES);
+    changesToInsert.forEach(
+      async value => await execQuery(INSERT_ACCOUNT_CHANGES, value),
+    );
   } catch (error) {
     console.error(error);
     connection.end();
